8caac26b6ebaa63852b21dc5d1fc9ac44b0e1479
NIPPA35 <ganhewagenipuna@gmail.com>
2026-01-13 20:17:10 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
     email: 'ganhewagenipuna11@gmail.com',
-    password: 'N!puna@20091111',
+    password: 'N!puna@2009',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
